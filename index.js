ee337f77e0f6fa2680948d7a64038ce3666b5180
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-01-08 21:37:38 +0100

    js_file initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const getData = (e) => {
+    const datas=[...e.target]
+
+    datas.map(data=>{
+        //console.log(data.type);
+        //data.type=="text" && data.value && console.log(`${data.id}: ${data.value}`);
+        //data.type=="email" && data.value && console.log(`${data.id}: ${data.value}`);
+        //data.type=="select-one" && data.value && console.log(`${data.id}: ${data.value}`);
+        //data.type=="radio" && data.checked && console.log(`${data.name}: ${data.value}`);
+        
+        data.type=="checkbox" && console.log(e);
+        data.type=="checkbox" && data.checked && console.log(`${data.name}: ${data.value}`);
+        //const type=data.name ? data.name : data.id;
+        //data.value && console.log(`${type}: ${data.value}`);
+    })
+}
+git 
+document.querySelector('form').addEventListener('submit', (e)=>{
+    e.preventDefault();
+    getData(e);
+});
